2dc7effd498c4423ee43964e044f163b96e0f633
muhammad irfan <mirfananjum727@gmail.com>
2026-06-13 01:33:07 +0500

    DEBUG UPDATE

--- src/techhub/settings.py
+++ src/techhub/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m)uev_=)g&99qlun9w%e4)myst_$m^y26!seq$kx4(lehmn%e3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app",
