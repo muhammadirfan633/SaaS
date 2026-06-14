7454b0bc12ea558923c25f578834b64f4a318828
muhammad irfan <mirfananjum727@gmail.com>
2026-06-15 00:59:24 +0500

    update templates and views for about, contact, and services pages

--- src/core/views.py
+++ src/core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+def contact(request):
+    return render(request, 'core/contact_us.html')
--- src/services/urls.py
+++ src/services/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
     # Define your URL patterns here
+    path('', views.services, name='services'),
 ]
